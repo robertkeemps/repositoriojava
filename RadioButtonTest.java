f125b5e89072b50d5d9aecb83dc4ed3561095b5a
robertkeemps <158859369+robertkeemps@users.noreply.github.com>
2024-03-20 14:10:06 -0400

    Add files via upload

---
+++ RadioButtonTest.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package radiobuttonframe;
+
+/**
+ *
+ * @author Aluno
+ */
+import javax.swing.JFrame;
+
+public class RadioButtonTest 
+{
+    
+    public static void main( String[] args)
+    {
+        RadioButtonFrame radioButtonFrame = new RadioButtonFrame();
+        radioButtonFrame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE);
+        radioButtonFrame.setSize( 300, 100);
+        radioButtonFrame.setVisible(true);
+    }
+    
+}
